08256262c5227fa258acff82d68744942e543c19
Riccardo Balbo <riccardo@forkforge.net>
2018-01-29 20:30:45 +0100

    Add default welcome tip of 100,  delay start (will help reducing connections in case of a broken build), bind rpc to localhost

--- init_reddconomy.sh
+++ init_reddconomy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 60
 source /reddconomy_updater.sh
 
 echo "Starting...."
@@ -8,7 +9,7 @@ downloadReddconomy Reddconomy.jar
 
 if [ ! -f /data/reddconomy.json ];
 then
-    echo "{\"bind_ip\":\"0.0.0.0\", \"sqlite-path\": \"/data/db2.sqlite\", \"coin\":\"$COIN\", \"coin_short\":\"$COIN_SHORT\"}" > /data/reddconomy.json
+    echo "{\"welcome_tip\":100.0,\"bind_ip\":\"0.0.0.0\", \"sqlite-path\": \"/data/db2.sqlite\", \"coin\":\"$COIN\", \"coin_short\":\"$COIN_SHORT\"}" > /data/reddconomy.json
 fi
 
 /opt/java/bin/java -jar Reddconomy.jar /data/reddconomy.json
--- init_testnet.sh
+++ init_testnet.sh
@@ -2,10 +2,12 @@
 #Force testnet
 
 echo "server=1
+bind=0.0.0.0:45444
 rpcuser=rpcuser
 rpcpassword=rpcpassword
 rpcport=45443
-rpcallowip=0.0.0.0/0
+rpcallowip=127.0.0.1
+rpcbind=127.0.0.1:45443
 keypool=1500" > "${DATA_DIR}/${CONFIG_FILE}"
 
 if [ "$COIN" = "reddcoin" ];
