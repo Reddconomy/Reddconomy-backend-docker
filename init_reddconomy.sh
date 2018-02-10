15e53ca9ab9f7e355c2dc1ceb118e7bb63a85ca7
Riccardo Balbo <riccardo@forkforge.net>
2018-02-10 18:50:12 +0100

    Update

--- init_reddconomy.sh
+++ init_reddconomy.sh
@@ -9,7 +9,23 @@ downloadReddconomy Reddconomy.jar
 
 if [ ! -f /data/reddconomy.json ];
 then
-    echo "{\"welcome_tip\":100.0,\"bind_ip\":\"0.0.0.0\", \"sqlite-path\": \"/data/db2.sqlite\", \"coin\":\"$COIN\", \"coin_short\":\"$COIN_SHORT\"}" > /data/reddconomy.json
+    echo "{
+        \"welcome_tip\":100.0,
+        \"bind_ip\":\"0.0.0.0\", 
+        \"sqlite-path\": \"/data/db2.sqlite\",
+         \"coin\":\"$COIN\", 
+         \"coin_short\":\"$COIN_SHORT\",
+    " > /data/reddconomy.json
+
+    if [ "$COIN" = "dogecoin" ];
+    then
+        echo "\"blockchain_fee\":\"1.0/kb\"" >> /data/reddconomy.json
+    elif [ "$COIN" = "reddcoin" ] ;
+    then
+        echo "\"blockchain_fee\":\"0.001/kb\"" >> /data/reddconomy.json
+    fi
+
+    echo "}"  >> /data/reddconomy.json
 fi
 
 /opt/java/bin/java -jar Reddconomy.jar /data/reddconomy.json
